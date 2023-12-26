9b58d22d461c1efc68e952c9da145b2ed3737bab
hgrsd <daniel@hgrsd.nl>
2023-12-26 09:21:33 +0000

    test join

--- src/core/transformations/inner_join.rs
+++ src/core/transformations/inner_join.rs
@@ -39,7 +39,8 @@ impl InnerJoin {
         );
         let apply = Box::new(move |left: &Dataframe, right: &Dataframe| {
             let right_by_key = Self::group_columns(&right_owned, right);
-            let mut joined_set: Dataframe = vec![];
+            let mut dataframe: Dataframe = vec![];
+
             for row in left.iter() {
                 if let Some(identifier) = row.get(&left_owned).and_then(extract_identifier) {
                     if let Some(matches) = right_by_key.get(&identifier) {
@@ -51,12 +52,12 @@ impl InnerJoin {
                             for (k, v) in *m {
                                 joined_row.insert(k.clone(), v.clone());
                             }
-                            joined_set.push(joined_row);
+                            dataframe.push(joined_row);
                         }
                     }
                 }
             }
-            joined_set
+            dataframe
         });
         InnerJoin { apply }
     }
@@ -65,5 +66,191 @@ impl InnerJoin {
 impl Transformation for InnerJoin {
     fn transform(&self, dfs: Vec<Dataframe>) -> Vec<Dataframe> {
         vec![(self.apply)(&dfs[0], &dfs[1])]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn no_matching_ids() {
+        let dfs = vec![
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id2"))),
+                    (String::from("foo"), ColumnValue::Integer(1)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id3"))),
+                    (String::from("foo"), ColumnValue::Integer(2)),
+                ]),
+            ],
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id4"))),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id5"))),
+                    (String::from("bar"), ColumnValue::Integer(4)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id6"))),
+                    (String::from("bar"), ColumnValue::Integer(5)),
+                ]),
+            ],
+        ];
+
+        let op = InnerJoin::new("id = id");
+
+        let result = op.transform(dfs);
+        assert_eq!(result[0], vec![],)
+    }
+
+    #[test]
+    fn no_matching_column_names() {
+        let dfs = vec![
+            vec![HashMap::from([
+                (String::from("id"), ColumnValue::String(String::from("id1"))),
+                (String::from("foo"), ColumnValue::Integer(0)),
+            ])],
+            vec![HashMap::from([
+                (String::from("id"), ColumnValue::String(String::from("id4"))),
+                (String::from("bar"), ColumnValue::Integer(3)),
+            ])],
+        ];
+
+        let op = InnerJoin::new("non_existing = non_existing");
+
+        let result = op.transform(dfs);
+        assert_eq!(result[0], vec![],)
+    }
+
+    #[test]
+    fn matching_rows() {
+        let dfs = vec![
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id2"))),
+                    (String::from("foo"), ColumnValue::Integer(1)),
+                ]),
+            ],
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id2"))),
+                    (String::from("bar"), ColumnValue::Integer(4)),
+                ]),
+            ],
+        ];
+
+        let op = InnerJoin::new("id = id");
+
+        let result = op.transform(dfs);
+        assert_eq!(
+            result[0],
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id2"))),
+                    (String::from("foo"), ColumnValue::Integer(1)),
+                    (String::from("bar"), ColumnValue::Integer(4)),
+                ])
+            ],
+        )
+    }
+
+    #[test]
+    fn multiple_matching_rows_right_multiplex() {
+        let dfs = vec![
+            vec![HashMap::from([
+                (String::from("id"), ColumnValue::String(String::from("id1"))),
+                (String::from("foo"), ColumnValue::Integer(0)),
+            ])],
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("bar"), ColumnValue::Integer(4)),
+                ]),
+            ],
+        ];
+
+        let op = InnerJoin::new("id = id");
+
+        let result = op.transform(dfs);
+        assert_eq!(
+            result[0],
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                    (String::from("bar"), ColumnValue::Integer(4)),
+                ]),
+            ],
+        )
+    }
+
+    #[test]
+    fn multiple_matching_rows_left_multiplex() {
+        let dfs = vec![
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(1)),
+                ]),
+            ],
+            vec![HashMap::from([
+                (String::from("id"), ColumnValue::String(String::from("id1"))),
+                (String::from("bar"), ColumnValue::Integer(3)),
+            ])],
+        ];
+
+        let op = InnerJoin::new("id = id");
+
+        let result = op.transform(dfs);
+        assert_eq!(
+            result[0],
+            vec![
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(0)),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+                HashMap::from([
+                    (String::from("id"), ColumnValue::String(String::from("id1"))),
+                    (String::from("foo"), ColumnValue::Integer(1)),
+                    (String::from("bar"), ColumnValue::Integer(3)),
+                ]),
+            ],
+        )
     }
 }
