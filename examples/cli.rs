ede17509eb9426adc8886e3d91f1e25b08b3f3ca
hgrsd <daniel@hgrsd.nl>
2023-12-26 14:03:33 +0000

    no result return type for now

--- examples/cli.rs
+++ examples/cli.rs
@@ -7,12 +7,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let parsed_filter_definition: PipelineDefinition =
         serde_json::from_str(&raw_filter_definition)?;
     let mut filter_engine = Engine::from_definition(parsed_filter_definition);
-    let filter_result = filter_engine.run()?;
+    let filter_result = filter_engine.run();
 
     let raw_join_definition = std::fs::read_to_string("examples/join.json")?;
     let parsed_join_definition: PipelineDefinition = serde_json::from_str(&raw_join_definition)?;
     let mut join_engine = Engine::from_definition(parsed_join_definition);
-    let join_result = join_engine.run()?;
+    let join_result = join_engine.run();
 
     println!(
         "filter: \n------\n{}\n------",
