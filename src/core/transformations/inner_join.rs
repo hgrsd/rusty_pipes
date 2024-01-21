9ea99b438d32eb7e82546a8ecb54b503c5a2f1f9
hgrsd <daniel@hgrsd.nl>
2024-01-21 22:45:17 +0000

    fixes

--- src/core/transformations/inner_join.rs
+++ src/core/transformations/inner_join.rs
@@ -6,20 +6,25 @@ use std::collections::HashMap;
 
 use super::Transformation;
 
-fn extract_identifier(from: &ColumnValue) -> Option<String> {
+fn extract_identifier(from: &ColumnValue) -> RustyPipesResult<String> {
     match from {
-        ColumnValue::String(s) => Some(s.clone()),
-        ColumnValue::Integer(i) => Some(i.to_string()),
-        _ => None,
+        ColumnValue::String(s) => Ok(s.clone()),
+        ColumnValue::Integer(i) => Ok(i.to_string()),
+        _ => Err(RustyPipesError::TransformationError(
+            "Only ints or strings can be used as identifiers".to_owned(),
+        )),
     }
 }
 
-fn group_rows<'b>(key: &str, df: &'b Dataframe) -> HashMap<String, Vec<&'b Row>> {
-    let grouped = df.iter().filter_map(|row| {
-        row.get(key)
-            .and_then(extract_identifier)
-            .map(|identifier| (identifier, vec![row]))
-    });
+fn group_rows<'b>(key: &str, df: &'b Dataframe) -> RustyPipesResult<HashMap<String, Vec<&'b Row>>> {
+    let mut grouped: Vec<(String, Vec<&Row>)> = vec![];
+    for row in df {
+        let value = row.get(key);
+        if let Some(v) = value {
+            let identifier = extract_identifier(v)?;
+            grouped.push((identifier, vec![row]));
+        }
+    }
 
     let mut result: HashMap<String, Vec<&Row>> = HashMap::new();
     for (identifier, rows) in grouped {
@@ -30,7 +35,7 @@ fn group_rows<'b>(key: &str, df: &'b Dataframe) -> HashMap<String, Vec<&'b Row>>
         }
     }
 
-    result
+    Ok(result)
 }
 
 /// Inner Join two data frames. This operation has an arity of two: it requires two dataframes to be provided as its
@@ -62,28 +67,31 @@ impl<'a> InnerJoin<'a> {
 
 impl Transformation for InnerJoin<'_> {
     fn transform(&self, dfs: &Vec<&Dataframe>) -> RustyPipesResult<Vec<Dataframe>> {
-        let right_rows_by_key = group_rows(self.right_key, dfs[1]);
-        let joined = dfs[0]
-            .iter()
-            .flat_map(|left_row| {
-                left_row
-                    .get(self.left_key)
-                    .and_then(extract_identifier)
-                    .and_then(|identifier| right_rows_by_key.get(&identifier))
+        let right_rows_by_key = group_rows(self.right_key, dfs[1])?;
+
+        let mut joined = vec![];
+
+        for row in dfs[0] {
+            let value = row.get(self.left_key);
+            if let Some(v) = value {
+                let identifier = extract_identifier(v)?;
+                let rows = right_rows_by_key
+                    .get(&identifier)
                     .map_or(vec![], |matching_rows| {
                         matching_rows
                             .iter()
                             .map(|matching_row| {
-                                left_row
-                                    .iter()
+                                row.iter()
                                     .chain(matching_row.iter())
                                     .map(|(key, value)| (key.clone(), value.clone()))
                                     .collect::<Row>()
                             })
                             .collect::<Vec<_>>()
-                    })
-            })
-            .collect();
+                    });
+                joined.extend(rows);
+            }
+        }
+
         Ok(vec![joined])
     }
 }
@@ -311,16 +319,16 @@ mod test {
     }
 
     #[test]
-    fn does_not_join_on_floats() {
+    fn join_on_float_errors() {
         let dfs = vec![
-            vec![HashMap::from([
-                (String::from("id"), ColumnValue::Decimal(1.0)),
-                (String::from("foo"), ColumnValue::Integer(0)),
-            ])],
-            vec![HashMap::from([
-                (String::from("id"), ColumnValue::Decimal(1.0)),
-                (String::from("bar"), ColumnValue::Integer(3)),
-            ])],
+            vec![HashMap::from([(
+                String::from("id"),
+                ColumnValue::Decimal(1.0),
+            )])],
+            vec![HashMap::from([(
+                String::from("id"),
+                ColumnValue::Decimal(1.0),
+            )])],
         ];
 
         let op = InnerJoin::new("id = id").unwrap();
@@ -328,7 +336,15 @@ mod test {
         let df_refs = dfs.iter().collect();
 
         let result = op.transform(&df_refs);
-        assert_eq!(result.unwrap()[0], vec![],)
+
+        assert!(result.is_err_and(|err| {
+            match err {
+                RustyPipesError::TransformationError(message) => {
+                    message.contains("Only ints or strings can be used as identifiers")
+                }
+                _ => false,
+            }
+        }))
     }
 
     #[test]
